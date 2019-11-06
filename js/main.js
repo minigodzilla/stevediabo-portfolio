6837cf2fd96229b19f807011a2f08943864e7124
Steve Diabo <stevediabo@gmail.com>
2019-11-06 13:36:18 -0500

    prototyping

--- js/main.js
+++ js/main.js
@@ -8,34 +8,47 @@ $(function () {
 
 	(() => {
 
+
 		const page1 = new ScrollMagic.Scene
 		({
 			triggerElement: '.sd-page-1', triggerHook: 0, duration: '200%'
-		// }).setPin(".sd-page-1")
-		}).setPin('.sd-page-1', { pushFollowers: false })
+		})
+		.setPin('.sd-page-1', { pushFollowers: false })
 		.addIndicators({name: 'pg1'})
+
+		const tween = TweenMax.to('.sd-stripe', 1, { className: '+=sd-animated', ease: Power0.easeNone });
+
+		const page1line1 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-1', triggerHook: 0, duration: '200%'
+		})
+		.setTween(tween)
 
 		const page2 = new ScrollMagic.Scene
 		({
 			triggerElement: '.sd-page-2', triggerHook: 0, duration: '100%'
-		}).setPin('.sd-page-2')
+		})
+		.setPin('.sd-page-2')
 		.addIndicators({name: 'pg2'})
 
 		const page3 = new ScrollMagic.Scene
 		({
 			triggerElement: '.sd-page-3', triggerHook: 0, duration: '100%'
-		}).setPin('.sd-page-3')
+		})
+		.setPin('.sd-page-3')
 		.addIndicators({name: 'pg3'})
 
 		const page4 = new ScrollMagic.Scene
 		({
 			triggerElement: '.sd-page-4', triggerHook: 0, duration: '100%'
-		}).setPin('.sd-page-4')
+		})
+		.setPin('.sd-page-4')
 		.addIndicators({name: 'pg4'})
 
 
 		// Add scenes to the controller
 		page1.addTo (scrollController);
+		page1line1.addTo (scrollController);
 		page2.addTo (scrollController);
 		page3.addTo (scrollController);
 		page4.addTo (scrollController);
