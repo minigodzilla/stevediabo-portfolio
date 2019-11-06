a1a21b2fbf0b1a1b7ee85bf145ef8da0a5387af1
Steve Diabo <stevediabo@gmail.com>
2019-11-06 01:25:46 -0500

    prototyping

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+$(function () {
+	// build scene
+	// var scene = new ScrollMagic.Scene( {
+	// 	triggerElement: ".sd-page-1",
+	// 	triggerHook: 0,
+	// 	duration: 1000
+	// })
+	// .setPin(".sd-page-1")
+	// .addIndicators({name: "1 (duration: 1000)"}) // add indicators (requires plugin)
+	// .addTo(controller);
+
+	// // build scene
+	// var scene = new ScrollMagic.Scene( {
+	// 	triggerElement: ".sd-page-2",
+	// 	triggerHook: 0,
+	// 	duration: 1000
+	// })
+	// .setPin(".sd-page-2")
+	// .addIndicators({name: "1 (duration: 1000)"}) // add indicators (requires plugin)
+	// .addTo(controller);
+
+	// // build scene
+	// var scene = new ScrollMagic.Scene( {
+	// 	triggerElement: ".sd-page-2",
+	// 	triggerHook: 0,
+	// 	duration: 1000
+	// })
+	// .setPin(".sd-page-2")
+	// .addIndicators({name: "1 (duration: 1000)"}) // add indicators (requires plugin)
+	// .addTo(controller);
+
+	// Create an instance of the Scroll Magic Controller
+	let scrollController = new ScrollMagic.Controller();
+
+	(() => {
+
+		const page1 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-1', triggerHook: 0, duration: '100%'
+		}).setPin(".sd-page-1")
+		// }).setPin(".sd-page-1", {pushFollowers: false})
+		.addIndicators({name: "pg1"})
+
+		const page2 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-2', triggerHook: 0, duration: '100%'
+		}).setPin(".sd-page-2")
+		.addIndicators({name: "pg2"})
+
+		const page3 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-3', triggerHook: 0, duration: '100%'
+		}).setPin(".sd-page-3")
+		.addIndicators({name: "pg3"})
+
+		const page4 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-4', triggerHook: 0, duration: '100%'
+		}).setPin(".sd-page-4")
+		.addIndicators({name: "pg4"})
+
+
+		// Add scenes to the controller
+		page1.addTo (scrollController);
+		page2.addTo (scrollController);
+		page3.addTo (scrollController);
+		page4.addTo (scrollController);
+
+	})();
+
+});
