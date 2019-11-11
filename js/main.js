c32ea08867b84e0d8ed3c2c2fe3a408ea1509703
Steve Diabo <stevediabo@gmail.com>
2019-11-10 23:27:01 -0500

    whole lotta magic

--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@ $(function () {
 
 	(() => {
 
-		// this is dreadful but it gets the job done for now
+		// locking down page heights based on initial 100vh
 
 		var n = $(".sd-lock-height");
 		n.length > 0 && n.css("height", n.height() + "px");
+
+		// TODO: remove duplication in scenes
 
 		// pg 1
 
@@ -19,11 +21,11 @@ $(function () {
 
 		const page1scene = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-1', triggerHook: 0, duration: (n.height() * 2)
+			triggerElement: '.sd-page-1', triggerHook: 0, duration: '200%'
 		})
 		.setPin('.sd-page-1', { pushFollowers: false })
 		.setTween(page1tween)
-		// .addIndicators({name: 'pg1'})
+		.addIndicators({name: 'pg1'})
 
 		// pg 2
 
@@ -31,11 +33,11 @@ $(function () {
 
 		const page2scene = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-2', triggerHook: 0, duration: n.height()
+			triggerElement: '.sd-page-2', triggerHook: 0, duration: '100%'
 		})
 		.setPin('.sd-page-2', { pushFollowers: false })
 		.setTween(page2tween)
-		// .addIndicators({name: 'pg2'})
+		.addIndicators({name: 'pg2'})
 
 		// pg 3
 
@@ -43,48 +45,51 @@ $(function () {
 
 		const page3scene1 = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-3', triggerHook: 0, duration: (n.height() * 5)
+			triggerElement: '.sd-page-3', triggerHook: 0, duration: '500%'
 		})
 		.setPin('.sd-page-3', { pushFollowers: false })
 		.setTween(page3tween1)
-		// .addIndicators({name: 'pg3 sc1'})
+		.addIndicators({name: 'pg3 sc1'})
 
 		const page3scene2timeline = new TimelineLite();
 			page3scene2timeline
-				.fromTo('.sd-page-3 .sd-detail-bg-box', 0.2, { scaleX: 0, ease: Power2.easeInOut }, { scaleX: 1, ease: Power2.easeInOut })
-				.fromTo('.sd-page-3 .sd-detail-fg-box', 0.2, { opacity: 0, ease: Power2.easeInOut }, { opacity: 1, ease: Power2.easeInOut })
-				.set('.sd-page-3 .sd-detail-title-box', { opacity: 0 });
+				.fromTo('.sd-page-3 .sd-bg-box', 0.2, { scaleX: 0, ease: Power2.easeInOut }, { scaleX: 1, ease: Power2.easeInOut })
+				.fromTo('.sd-page-3 .sd-fg-box', 0.2, { opacity: 0, ease: Power2.easeInOut }, { opacity: 1, ease: Power2.easeInOut })
+				.set('.sd-page-3 .sd-title-box', { opacity: 0 })
+			;
 
 		const page3scene2 = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-3', triggerHook: 0, offset: (n.height() * 1.5)
+			triggerElement: '.sd-trigger-page-3-scene-2', triggerHook: 0
 		})
-		.setTween(page3scene2timeline)		
-		// .addIndicators({name: 'pg3 sc2'})
+		.setTween(page3scene2timeline)
+		.addIndicators({name: 'pg3 sc2'})
 
 		const page3scene3timeline = new TimelineLite();
 			page3scene3timeline
-				.fromTo('.sd-page-3 .sd-detail-fg-box .sd-bezel-laptop', 0.2, { opacity: 1, ease: Power2.easeInOut }, { opacity: 0, ease: Power2.easeInOut })
-				.fromTo('.sd-page-3 .sd-detail-fg-box .sd-bezel-phone', 0.2, { opacity: 0, ease: Power2.easeInOut }, { opacity: 1, ease: Power2.easeInOut });
+				.fromTo('.sd-page-3 .sd-fg-box .sd-bezel-laptop', 0.2, { opacity: 1, ease: Power2.easeInOut }, { opacity: 0, ease: Power2.easeInOut })
+				.fromTo('.sd-page-3 .sd-fg-box .sd-bezel-phone', 0.2, { opacity: 0, ease: Power2.easeInOut }, { opacity: 1, ease: Power2.easeInOut })
+			;
 
 		const page3scene3 = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-3', triggerHook: 0, offset: (n.height() * 2.5)
+			triggerElement: '.sd-trigger-page-3-scene-3', triggerHook: 0
 		})
-		.setTween(page3scene3timeline)		
-		// .addIndicators({name: 'pg3 sc3'})
+		.setTween(page3scene3timeline)
+		.addIndicators({name: 'pg3 sc3'})
 
 		const page3scene4timeline = new TimelineLite();
 			page3scene4timeline
-				.to('.sd-page-3 .sd-detail-fg-box', 0.2, { opacity: 0, ease: Power2.easeInOut })
-				.to('.sd-page-3 .sd-detail-bg-box', 0.2, { scaleX: 0, ease: Power2.easeInOut });
+				.fromTo('.sd-page-3 .sd-fg-box', 0.2, { opacity: 1, ease: Power2.easeInOut, immediateRender: false }, { opacity: 0, ease: Power2.easeInOut })
+				.fromTo('.sd-page-3 .sd-bg-box', 0.2, { scaleX: 1, ease: Power2.easeInOut, immediateRender: false }, { scaleX: 0, ease: Power2.easeInOut })
+			;
 
 		const page3scene4 = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-3', triggerHook: 0, offset: (n.height() * 3.5)
+			triggerElement: '.sd-trigger-page-3-scene-4', triggerHook: 0
 		})
 		.setTween(page3scene4timeline)		
-		// .addIndicators({name: 'pg3 sc4'})
+		.addIndicators({name: 'pg3 sc4'})
 
 		// pg 4
 
@@ -92,11 +97,11 @@ $(function () {
 
 		const page4scene = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-4', triggerHook: 0, duration: n.height()
+			triggerElement: '.sd-page-4', triggerHook: 0, duration: '100%'
 		})
 		.setPin('.sd-page-4', { pushFollowers: false })
 		.setTween(page4tween)
-		// .addIndicators({name: 'pg4'})
+		.addIndicators({name: 'pg4'})
 
 
 		// Add scenes to the controller
