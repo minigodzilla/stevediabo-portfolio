69f67ff7f0d624675c5553ba06ce6dc2d8117bd7
Steve Diabo <stevediabo@gmail.com>
2019-12-06 01:36:12 -0500

    infusion and nventive content

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ $(function () {
 
 		const page2scene1 = new ScrollMagic.Scene
 		({
-			triggerElement: '.sd-page-2 .sd-trigger-scene-1', triggerHook: 0, reverse: true
+			triggerElement: '.sd-page-2 .sd-trigger-scene-1', triggerHook: 0, reverse: false
 		})
 		.setTween(page2scene1tween)
 
@@ -119,28 +119,6 @@ $(function () {
 		})
 		.setClassToggle('.sd-page-3 .sd-xs-bezel-container .sd-bezel-phone', 'sd-show-overlay')		
 
-		// pg 4
-
-		const page4scene1tween = TweenMax.to('.sd-page-4 .sd-box', 1, { className: '+=sd-animated', ease: Linear.easeNone });
-
-		const page4scene1 = new ScrollMagic.Scene
-		({
-			triggerElement: '.sd-page-4 .sd-trigger-scene-1', triggerHook: 0, duration: '100%'
-		})
-		.setTween(page4scene1tween)
-
-		const page4scene1a = new ScrollMagic.Scene
-		({
-			triggerElement: '.sd-page-4 .sd-trigger-scene-1', triggerHook: 0
-		})
-		.setClassToggle('.sd-page-4', 'sd-past-top')
-
-		const page4scene3 = new ScrollMagic.Scene
-		({
-			triggerElement: '.sd-page-4 .sd-trigger-scene-3', triggerHook: 1
-		})
-		.setClassToggle('.sd-page-4', 'sd-past-bottom')
-
 		// pg 5
 
 		const page5scene1tween = TweenMax.to('.sd-page-5 .sd-animated-item', 1, { className: '+=sd-animated', ease: Linear.easeNone });
@@ -208,6 +186,107 @@ $(function () {
 		})
 		.setClassToggle('.sd-page-5 .sd-xs-bezel-container .sd-bezel-phone', 'sd-show-overlay')		
 
+		// pg 7
+
+		const page7scene1tween = TweenMax.to('.sd-page-7 .sd-animated-item', 1, { className: '+=sd-animated', ease: Linear.easeNone });
+
+		const page7scene1 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-7 .sd-trigger-scene-1', triggerHook: 1, duration: '350%'
+		})
+		.setTween(page7scene1tween)
+
+		const page7scene1a = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-7 .sd-trigger-scene-1', triggerHook: 1
+		})
+		.setClassToggle('.sd-page-7', 'sd-in-view')
+
+		const page7scene1b = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-7 .sd-trigger-scene-1', triggerHook: 0
+		})
+		.setClassToggle('.sd-page-7', 'sd-past-top')
+
+		const page7scene3 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-7 .sd-trigger-scene-3', triggerHook: 1
+		})
+		.setClassToggle('.sd-page-7', 'sd-past-bottom')		
+
+		const page7scene4 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-7 .sd-xs-bezel-container .sd-bezel .sd-overlay .sd-svg-play-icon path', triggerHook: 0.6, reverse: true
+		})
+		.setClassToggle('.sd-page-7 .sd-xs-bezel-container .sd-bezel', 'sd-show-overlay')
+
+		// pg 9
+
+		const page9scene1tween = TweenMax.to('.sd-page-9 .sd-animated-item', 1, { className: '+=sd-animated', ease: Linear.easeNone });
+
+		const page9scene1 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-9 .sd-trigger-scene-1', triggerHook: 1, duration: '350%'
+		})
+		.setTween(page9scene1tween)
+
+		const page9scene1a = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-9 .sd-trigger-scene-1', triggerHook: 1
+		})
+		.setClassToggle('.sd-page-9', 'sd-in-view')
+
+		const page9scene1b = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-9 .sd-trigger-scene-1', triggerHook: 0
+		})
+		.setClassToggle('.sd-page-9', 'sd-past-top')
+
+		const page9scene2timeline = new TimelineLite();
+			page9scene2timeline
+				.fromTo('.sd-page-9 .sd-lg-bezel-container .sd-bezel-winphone', 0.2, { 
+					opacity: 1,
+					pointerEvents: 'none',
+					immediateRender: false
+				},
+				{ 
+					opacity: 0,
+					pointerEvents: 'auto'
+				})
+				.fromTo('.sd-page-9 .sd-lg-bezel-container .sd-bezel-surface', 0.2, { 
+					opacity: 0,
+					pointerEvents: 'none'
+				},
+				{ 
+					opacity: 1,
+					pointerEvents: 'auto'
+				})
+			;
+
+		const page9scene2 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-9 .sd-trigger-scene-2', triggerHook: 0.5
+		})
+		.setTween(page9scene2timeline)
+
+		const page9scene3 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-9 .sd-trigger-scene-3', triggerHook: 1
+		})
+		.setClassToggle('.sd-page-9', 'sd-past-bottom')		
+
+		const page9scene4 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-9 .sd-xs-bezel-container .sd-bezel-winphone .sd-overlay .sd-svg-play-icon path', triggerHook: 0.6, reverse: true
+		})
+		.setClassToggle('.sd-page-9 .sd-xs-bezel-container .sd-bezel-winphone', 'sd-show-overlay')		
+
+		const page9scene5 = new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-9 .sd-xs-bezel-container .sd-bezel-surface .sd-overlay .sd-svg-play-icon path', triggerHook: 0.6, reverse: true
+		})
+		.setClassToggle('.sd-page-9 .sd-xs-bezel-container .sd-bezel-surface', 'sd-show-overlay')		
+
 		// Add scenes to the controller
 		page1scene1.addTo (scrollController);
 		page1scene2.addTo (scrollController);
@@ -219,9 +298,6 @@ $(function () {
 		page3scene3.addTo (scrollController);
 		page3scene4.addTo (scrollController);
 		page3scene5.addTo (scrollController);
-		page4scene1.addTo (scrollController);
-		page4scene1a.addTo (scrollController);
-		page4scene3.addTo (scrollController);
 		page5scene1.addTo (scrollController);
 		page5scene1a.addTo (scrollController);
 		page5scene1b.addTo (scrollController);
@@ -229,6 +305,18 @@ $(function () {
 		page5scene3.addTo (scrollController);
 		page5scene4.addTo (scrollController);
 		page5scene5.addTo (scrollController);
+		page7scene1.addTo (scrollController);
+		page7scene1a.addTo (scrollController);
+		page7scene1b.addTo (scrollController);
+		page7scene3.addTo (scrollController);
+		page7scene4.addTo (scrollController);
+		page9scene1.addTo (scrollController);
+		page9scene1a.addTo (scrollController);
+		page9scene1b.addTo (scrollController);
+		page9scene2.addTo (scrollController);
+		page9scene3.addTo (scrollController);
+		page9scene4.addTo (scrollController);
+		page9scene5.addTo (scrollController);
 
 		// Smooth Scroll
 		// var scroll = new SmoothScroll('a[href*="#"]', {
