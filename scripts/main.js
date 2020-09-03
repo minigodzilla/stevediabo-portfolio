450d237a657e4fb1e71c5a9e669807b07282bf7e
Steve Diabo <stevediabo@gmail.com>
2020-09-03 00:06:20 -0400

    removed all duplicated scrollmagic/gsap stuff

--- scripts/main.js
+++ scripts/main.js
@@ -61,17 +61,30 @@
 
 	});
 
-	// throttle the frames-per-second
-	//TweenMax.ticker.fps(40);
+	$('.sd-page-simple-title').each(function () {
 
-	// TODO: remove duplication in scenes
+		var $this = $(this);
+		var i = $this.attr('sd-page');
+
+		const tween = TweenMax.to('.sd-page-'+i+' .sd-color', 1, { className: '+=sd-animated' });
+
+		new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-'+i+' .sd-trigger-scene-1', triggerHook: 0.5, reverse: false
+		})
+		.setTween(tween)
+		.addTo(controller);
+	});
 
 	$('.sd-page-work-detail').each(function () {
 
 		var $this = $(this);
 		var i = $this.attr('sd-page');
 
-		const tween = TweenMax.to('.sd-page-'+i+' .sd-stripe-area, .sd-page-'+i+' .sd-stripe-area .sd-stripe', 1, { className: '+=sd-animated', ease: Linear.easeNone });
+
+		// animating stripe areas and stripes
+
+		const stripeTween = TweenMax.to('.sd-page-'+i+' .sd-stripe-area, .sd-page-'+i+' .sd-stripe-area .sd-stripe', 1, { className: '+=sd-animated', ease: Linear.easeNone });
 
 		new ScrollMagic.Scene
 		({
@@ -79,8 +92,10 @@
 			triggerHook: 1,
 			duration: '350%'
 		})
-		.setTween(tween)
+		.setTween(stripeTween)
 		.addTo(controller);
+
+		// adding classes that control bezel container positioning on desktop
 
 		new ScrollMagic.Scene
 		({
@@ -103,7 +118,39 @@
 		.setClassToggle('.sd-page-'+i, 'sd-past-bottom')
 		.addTo(controller);
 
+		// fade in/out multiple bezels on desktop, if present
+
+		const bezelTimeline = new TimelineLite();
+			bezelTimeline
+				.fromTo('.sd-page-'+i+' .sd-bezel-container .sd-bezel-1', 0.2, { 
+					opacity: 1,
+					pointerEvents: 'auto',
+					immediateRender: false
+				},
+				{ 
+					opacity: 0,
+					pointerEvents: 'none'
+				})
+				.fromTo('.sd-page-'+i+' .sd-bezel-container .sd-bezel-2', 0.2, { 
+					opacity: 0,
+					pointerEvents: 'none'
+				},
+				{ 
+					opacity: 1,
+					pointerEvents: 'auto'
+				},"=-0.2")
+			;
+
+		new ScrollMagic.Scene
+		({
+			triggerElement: '.sd-page-'+i+' .sd-trigger-scene-2', triggerHook: 0.5
+		})
+		.setTween(bezelTimeline)
+		.addTo(controller);
+
 	});
+
+	// animate bezel container background image position on mobile
 
 	$('.sd-page-work-detail .sd-bezel-container').each(function () {
 
@@ -123,7 +170,7 @@
 
 	// pg 1
 
-	const page1scene1tween = TweenMax.to('.sd-page-1, .sd-page-1 .sd-animated-item', 1, { className: '+=sd-animated', ease: Linear.easeNone });
+	const page1scene1tween = TweenMax.to('.sd-page-1, .sd-page-1 .sd-stripe-area .sd-stripe', 1, { className: '+=sd-animated', ease: Linear.easeNone });
 
 	const page1scene1 = new ScrollMagic.Scene
 	({
@@ -157,200 +204,33 @@
 
 	// pg 2
 
-	const page2scene0timeline = new TimelineLite();
-		page2scene0timeline
-		.fromTo('.sd-page-2 .sd-color', 0.2, { 
-			opacity: '0'
-		},
-		{
-			opacity: '1'
-		})
-		.fromTo('.sd-page-2 .sd-photo', 0.4, { 
-			transform: 'translate3d(200%,5%,0) rotate(-260deg) scale(0.85)'
-		},
-		{
-			transform: 'translate3d(0%,0%,0) rotate(4deg) scale(0.85)'
-		})
-		;
-
-	const page2scene0 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-2 .sd-trigger-scene-1', triggerHook: 0.5, reverse: false
-	})
-	.setTween(page2scene0timeline)
-	.addTo(controller);
-
-	const page2scene1tween = TweenMax.to('.sd-page-2 .sd-signature', 1, { className: '+=sd-animated' });
-
-	const page2scene1 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-2 .sd-signature', triggerHook: 0.75, reverse: false
-	})
-	.setTween(page2scene1tween)
-	.addTo(controller);
-
-	// pg 3
-
-	const page3scene2timeline = new TimelineLite();
-		page3scene2timeline
-			.fromTo('.sd-page-3 .sd-bezel-container .sd-bezel-laptop', 0.2, { 
-				opacity: 1,
-				pointerEvents: 'auto',
-				immediateRender: false
+	const photoTimeline = new TimelineLite();
+		photoTimeline
+			.fromTo('.sd-photo', 0.4, { 
+				transform: 'translate3d(200%,5%,0) rotate(-260deg) scale(0.85)'
 			},
-			{ 
-				opacity: 0,
-				pointerEvents: 'none'
-			})
-			.fromTo('.sd-page-3 .sd-bezel-container .sd-bezel-phone', 0.2, { 
-				opacity: 0,
-				pointerEvents: 'none'
-			},
-			{ 
-				opacity: 1,
-				pointerEvents: 'auto'
-			},"=-0.2")
-		;
-
-	const page3scene2 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-3 .sd-trigger-scene-2', triggerHook: 0.5
-	})
-	.setTween(page3scene2timeline)
-	.addTo(controller);
-
-	// pg 4
-
-	const page4scene0tween = TweenMax.to('.sd-page-4 .sd-color', 1, { className: '+=sd-animated' });
-
-	const page4scene0 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-4 .sd-trigger-scene-1', triggerHook: 0.5, reverse: false
-	})
-	.setTween(page4scene0tween)
-	.addTo(controller);
-
-	// pg 5
-
-	const page5scene2timeline = new TimelineLite();
-		page5scene2timeline
-			.fromTo('.sd-page-5 .sd-bezel-container .sd-bezel-laptop', 0.2, { 
-				opacity: 1,
-				pointerEvents: 'none',
-				immediateRender: false
-			},
-			{ 
-				opacity: 0,
-				pointerEvents: 'auto'
-			})
-			.fromTo('.sd-page-5 .sd-bezel-container .sd-bezel-phone', 0.2, { 
-				opacity: 0,
-				pointerEvents: 'none'
-			},
-			{ 
-				opacity: 1,
-				pointerEvents: 'auto'
-			},"=-0.2")
-		;
-
-	const page5scene2 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-5 .sd-trigger-scene-2', triggerHook: 0.5
-	})
-	.setTween(page5scene2timeline)
-	.addTo(controller);
-
-	// pg 6
-
-	const page6scene0tween = TweenMax.to('.sd-page-6 .sd-color', 1, { className: '+=sd-animated' });
-
-	const page6scene0 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-6 .sd-trigger-scene-1', triggerHook: 0.5, reverse: false
-	})
-	.setTween(page6scene0tween)
-	.addTo(controller);
-
-	// pg 7
-
-	// pg 8
-
-	const page8scene0tween = TweenMax.to('.sd-page-8 .sd-color', 1, { className: '+=sd-animated' });
-
-	const page8scene0 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-8 .sd-trigger-scene-1', triggerHook: 0.5, reverse: false
-	})
-	.setTween(page8scene0tween)
-	.addTo(controller);
-
-	// pg 9
-
-	const page9scene2timeline = new TimelineLite();
-		page9scene2timeline
-			.fromTo('.sd-page-9 .sd-bezel-container .sd-bezel-winphone', 0.2, { 
-				opacity: 1,
-				pointerEvents: 'none',
-				immediateRender: false
-			},
-			{ 
-				opacity: 0,
-				pointerEvents: 'auto'
-			})
-			.fromTo('.sd-page-9 .sd-bezel-container .sd-bezel-surface', 0.2, { 
-				opacity: 0,
-				pointerEvents: 'none'
-			},
-			{ 
-				opacity: 1,
-				pointerEvents: 'auto'
-			},"=-0.2")
-		;
-
-	const page9scene2 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-9 .sd-trigger-scene-2', triggerHook: 0.5
-	})
-	.setTween(page9scene2timeline)
-	.addTo(controller);
-
-	// pg 10
-
-	const page10scene0tween = TweenMax.to('.sd-page-10 .sd-color', 1, { className: '+=sd-animated' });
-
-	const page10scene0 = new ScrollMagic.Scene
-	({
-		triggerElement: '.sd-page-10 .sd-trigger-scene-1', triggerHook: 0.5, reverse: false
-	})
-	.setTween(page10scene0tween)
-	.addTo(controller);
-
-	// pg 11
-
-	//const page11scene1tween = TweenMax.to('.sd-page-11, .sd-page-11 .sd-animated-item', 1, { className: '+=sd-animated', ease: Linear.easeNone });
-
-	// const page11scene1 = new ScrollMagic.Scene
-	// ({
-	// 	triggerElement: '.sd-page-11', triggerHook: 0, duration: '200%'
-	// })
-	// .setPin('.sd-page-11', { pushFollowers: true })
-	// .setTween(page11scene1tween)
-	// .addTo(controller);
-
-	const page11scene2timeline = new TimelineLite();
-		page11scene2timeline
-			.to('.sd-page-11 .sd-box .sd-name-container', 0.2, { 
-				className: '+=sd-animated'
+			{
+				transform: 'translate3d(0%,0%,0) rotate(4deg) scale(0.85)'
 			})
 		;
 
-	// const page11scene2 = new ScrollMagic.Scene
-	// ({
-	// 	triggerElement: '.sd-page-11', triggerHook: 0
-	// })
-	// .setTween(page11scene2timeline)
-	// .addTo(controller);
+	const photoScene = new ScrollMagic.Scene
+	({
+		triggerElement: '.sd-photo', triggerHook: 0.5, reverse: false
+	})
+	.setTween(photoTimeline)
+	.addTo(controller);
 
+	const signatureTween = TweenMax.to('.sd-signature', 1, { className: '+=sd-animated' });
+
+	const signatureScene = new ScrollMagic.Scene
+	({
+		triggerElement: '.sd-signature', triggerHook: 0.75, reverse: false
+	})
+	.setTween(signatureTween)
+	.addTo(controller);
+
+	// video controls
 
 	$('.sd-screen-content-video').one('click', function() {
 
