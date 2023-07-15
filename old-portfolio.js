2391ed5be5e26b17eab193a9beadb42fdb2bf23f
Steve Diabo <135646014+stevediabo-ndconnect@users.noreply.github.com>
2023-07-15 15:44:56 -0400

    fix up that old server

--- old-portfolio.js
+++ old-portfolio.js
@@ -8,22 +8,6 @@ process.env.NODE_ENV = 'production';
 
 // app.use(express.static('public'));
 
-const excludedPaths = ['/css', '/fonts', '/img', '/js', '/scripts', '/video', '/js'];
-
-// Catch-all route for all paths except excluded ones
-app.get('*', (req, res) => {
-  const shouldExclude = excludedPaths.some((path) => req.url.startsWith(path));
-
-  if (shouldExclude) {
-    // Pass through to the next middleware or route handler
-  }
-
-  const rewrittenUrl = `/#${req.url.substring(1)}`;
-  res.redirect(rewrittenUrl);
-});
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
+app.use(express.static(path.join(__dirname, '')));
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
