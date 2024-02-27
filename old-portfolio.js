ff3f7657a239221aff297932b1ef9091c2280c0d
Steve Diabo <135646014+stevediabo-ndconnect@users.noreply.github.com>
2024-02-27 01:21:57 -0500

    node server!

--- old-portfolio.js
+++ old-portfolio.js
@@ -10,4 +10,17 @@ process.env.NODE_ENV = 'production';
 
 app.use(express.static(path.join(__dirname, '')));
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+
+  (async () => {
+    try {
+      const open = (await import('open')).default;
+      await open(`http://localhost:${PORT}`);
+    } catch (error) {
+      console.error('Error importing/opening browser:', error);
+    }
+  })();
+
+
+});
